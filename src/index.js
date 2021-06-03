aa27f770b488df51cc1aad3e557de061493b5be5
Tania Tatula <tgrogol@gmail.com>
2021-06-03 22:53:15 +0300

    додає маску на телефон

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import './js/menu';
 import './js/goods';
 import './js/thanks';
 import './js/language';
+import './js/mask';
 
 
---
+++ src/js/mask.js
@@ -0,0 +1,10 @@
+import IMask from 'imask';
+
+var phoneMask = IMask(
+    document.getElementById('phone-mask'), {
+      mask: '+{38}(000)000-00-00'
+    });
+
+  
+
+    
